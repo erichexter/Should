410e0000e391bde674ef47efc642751f5d115471
Eivind Gussiås Løkseth <eloekset@msn.com>
2017-02-25 22:42:09 +0100

    Upgrade build script to build DotNetCore solution.

--- build.bat
+++ build.bat
@@ -1,4 +1,5 @@
 tools\nuget.exe install MSBuildTasks -o .build\
 tools\nuget.exe install xunit.runner.msbuild -o .build\
 tools\nuget.exe restore src\Should.sln
+tools\nuget.exe restore src\Should.DotNetCore.sln
 msbuild.exe ms.build
