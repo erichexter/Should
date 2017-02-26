ee79d1806ac54bf2b1a6eb51e1d7a33a61b02bcc
AlSki <al@alski.net>
2017-02-26 12:28:24 +0000

    Hack: Hardcode the version of msbuild community tasks in build.bat until there is a way to inject the version into ms.build

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
-tools\nuget.exe install MSBuildTasks -o .build\
+tools\nuget.exe install MSBuildTasks -o .build\ -version 1.5.0.214 
 tools\nuget.exe install xunit.runner.msbuild -o .build\
 tools\nuget.exe restore src\Should.sln
 tools\nuget.exe restore src\Should.DotNetCore.sln
